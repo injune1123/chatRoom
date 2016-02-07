921325fc61d0be5d08b3ee3d7e55b5077dbca761
injune1123 <injune1123@Juns-Air.home>
2016-02-06 22:26:06 -0500

    made text disappear in the textarea after being send

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 
     var username = prompt("WELCOME! WELCOME! WELCOME~", "Waht would you like to be called?") || "mysterious user";
 
-    $("#userList").append(username);
-    $("#chatbox").append(username + " joined the chat ...");
+    $("#userList").append("<span>" + username + "</span>");
+    $("#chatbox").append("<span>" + username + "</span>" + " joined the chat ...");
 
     var user = new Parse.User();
 
@@ -12,16 +12,15 @@ $(document).ready(function() {
 
         var text = $("textarea[name='message']").val();
         var date = new Date();
-		var time = " @ "  + date.getHours() + ":"+ date.getMinutes() + ":" + date.getSeconds();
-
-       	$("#chatbox").append( "<br>" + time  + " <span>" + username + "</span> said : <span>" + text +"</span>");
+        var time = " @ " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        $('textarea').val('');
+        $("#chatbox").append("<br>" + time + " <span>" + username + "</span> said : <span>" + text + "</span>");
         user.set('username', username);
         user.set('text', text);
-        user.set('time', date.getTime()	);
+        user.set('time', date.getTime());
         console.log(user);
-
-
     })
+    
 
     // user.set('',text)
 })
